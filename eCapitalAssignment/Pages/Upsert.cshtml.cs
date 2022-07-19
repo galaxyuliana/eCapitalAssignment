d9aea5607c66af06b87c1a1d9c375551a39f0acd
galaxyuliana <yulianashin613@gmail.com>
2022-07-19 14:47:13 -0700

    Added Upsert page

---
+++ eCapitalAssignment/Pages/Upsert.cshtml.cs
@@ -0,0 +1,52 @@
+using eCapitalAssignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eCapitalAssignment.Pages
+{
+    public class UpsertModel : PageModelBase
+    {
+        [BindProperty]
+        public Employee Employee { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id != null)
+            {
+                var selectedEmployee = await GetEmployeeAsync((int)id);
+
+                if (selectedEmployee == null)
+                {
+                    return NotFound();
+                }
+
+                Employee = selectedEmployee;
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if (id == null)
+            {
+                await AddEmployee(Employee);
+            }
+            else if (id > 0 && Employee != null)
+            {
+                await UpdateEmployee((int)id, Employee);
+            }
+            else
+            {
+                return Redirect("/");
+            }
+
+            return Redirect("/");
+        }
+    }
+}
